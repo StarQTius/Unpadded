8ce3fcd6c6c62d9d3623e6355c7e9f8893c861cc
StarQTius <dussautoirp@gmail.com>
2022-04-09 23:53:26 +0200

    Put remove_cv_ref_t where needed

--- include/k2o/detail/type_traits/input_tuple.hpp
+++ include/k2o/detail/type_traits/input_tuple.hpp
@@ -2,11 +2,10 @@
 
 #pragma once
 
-#include <type_traits>
-
 #include <upd/format.hpp>
 #include <upd/tuple.hpp>
 
+#include "remove_cv_ref.hpp"
 #include "signature.hpp"
 
 namespace k2o {
@@ -17,8 +16,7 @@ struct input_tuple_impl;
 
 template<typename R, typename... Args, upd::endianess Endianess, upd::signed_mode Signed_Mode>
 struct input_tuple_impl<R(Args...), Endianess, Signed_Mode> {
-  using type =
-      upd::tuple<Endianess, Signed_Mode, typename std::remove_cv<typename std::remove_reference<Args>::type>::type...>;
+  using type = upd::tuple<Endianess, Signed_Mode, remove_cv_ref_t<Args>...>;
 };
 
 //! \brief Template instance of `upd::tuple` suitable for holding the parameters of an invocable of type `F`
