f48b588fab56e2b8aee5716ae9f65d6a0a90ece6
StarQTius <dussautoirp@gmail.com>
2022-04-14 19:37:43 +0200

    Overhaul flist.hpp

--- include/k2o/flist.hpp
+++ include/k2o/flist.hpp
@@ -7,14 +7,19 @@ namespace k2o {
 template<typename...>
 struct flist_t;
 
+//! \brief Typelist holding references to objects with static storage duration
+//! \tparam Functions Invocable objects bound by reference
 template<typename... Fs, Fs... Functions>
 struct flist_t<unevaluated<Fs, Functions>...> {};
 
 #if __cplusplus >= 201703L
+//! \copydoc flist_t
 template<auto &...Functions>
 flist_t<unevaluated<decltype(Functions), Functions>...> flist;
 #endif // __cplusplus >= 201703L
 
+//! \brief Make a typelist of references to invocable objects
+//! \related flist_t
 template<typename... Fs, Fs... Functions>
 constexpr flist_t<unevaluated<Fs, Functions>...> make_flist(unevaluated<Fs, Functions>...) {
   return {};
