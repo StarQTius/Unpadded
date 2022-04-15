cb7fe5e335f61cdd79dd9c711d7f98fdfd8ed0e0
StarQTius <dussautoirp@gmail.com>
2022-04-15 23:32:57 +0200

    Clean doc in policy.hpp

--- include/k2o/policy.hpp
+++ include/k2o/policy.hpp
@@ -1,5 +1,4 @@
 //! \file
-//! \brief Class behavior tunability
 
 #pragma once
 
@@ -11,12 +10,14 @@ namespace k2o {
 enum class order_features { STATIC_STORAGE_DURATION_ONLY, ANY };
 
 //! \brief Value holder to help deduce order features
+//! \tparam Order_Features Features to hold
 template<order_features Order_Features>
 using order_features_h = std::integral_constant<order_features, Order_Features>;
 
 namespace policy {
 
 //! \brief Ensures that stored orders have static storage duration
+//!
 //! In that case, the containing object may refer to orders rather than managing memory space for them.
 constexpr order_features_h<order_features::STATIC_STORAGE_DURATION_ONLY> static_storage_duration_only;
 
