d6198e37b751bc035cf2927fc444559df47384cf
StarQTius <dussautoirp@gmail.com>
2023-02-19 00:00:06 +0100

    Implement platform optimization for serialization

--- include/upd/upd.hpp
+++ include/upd/upd.hpp
@@ -1,23 +1,32 @@
 #pragma once
 
+#include "format.hpp"
+
 #define UPD_FWD(x) static_cast<decltype(x) &&>(x)
 #define UPD_PACK(...) __VA_ARGS__
+#define UPD_SCOPE_OPERATOR(LHS, RHS) LHS::RHS
 
 namespace upd {
-enum class endianess;
-enum class signed_mode;
-
-constexpr class {
-public:
+constexpr struct {
+#if defined(UPD_PLATFORM_ENDIANESS)
+  upd::endianess endianess = UPD_SCOPE_OPERATOR(upd::endianess, UPD_PLATFORM_ENDIANESS);
+#else  // defined(UPD_PLATFORM_ENDIANESS)
   struct {
     constexpr bool operator==(endianess) const { return false; }
     constexpr bool operator!=(endianess) const { return true; }
   } endianess;
+#endif // defined(UPD_PLATFORM_ENDIANESS)
+
+#if defined(UPD_PLATFORM_SIGNED_MODE)
+  upd::signed_mode signed_mode = UPD_SCOPE_OPERATOR(upd::signed_mode, UPD_PLATFORM_SIGNED_MODE);
+#else  // defined(UPD_PLATFORM_SIGNED_MODE)
   struct {
     constexpr bool operator==(signed_mode) const { return false; }
     constexpr bool operator!=(signed_mode) const { return true; }
   } signed_mode;
+#endif // defined(UPD_PLATFORM_SIGNED_MODE)
 } platform_info;
+
 } // namespace upd
 
 template<typename>
