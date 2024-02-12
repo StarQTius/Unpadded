c46c23f106c0add596c74c0512e6e906d2956f53
StarQTius <>
2024-02-12 23:13:02 +0100

    Replace long long by intmax_t in serializer_interface

--- test/basic_ibytestream.cpp
+++ test/basic_ibytestream.cpp
@@ -25,10 +25,10 @@ struct serializer_interface {
   serializer_interface(serializer_interface &&) = delete;
   auto operator=(serializer_interface &&) -> serializer_interface & = delete;
 
-  virtual void serialize_unsigned(unsigned long long value, std::size_t size, std::byte *output) = 0;
-  virtual void serialize_signed(long long value, std::size_t size, std::byte *output) = 0;
-  virtual auto deserialize_unsigned(const std::byte *input, std::size_t size) -> unsigned long long = 0;
-  virtual auto deserialize_signed(const std::byte *input, std::size_t size) -> long long = 0;
+  virtual void serialize_unsigned(std::uintmax_t value, std::size_t size, std::byte *output) = 0;
+  virtual void serialize_signed(std::intmax_t value, std::size_t size, std::byte *output) = 0;
+  virtual auto deserialize_unsigned(const std::byte *input, std::size_t size) -> std::uintmax_t = 0;
+  virtual auto deserialize_signed(const std::byte *input, std::size_t size) -> std::intmax_t = 0;
   virtual ~serializer_interface() = default;
 };
 
