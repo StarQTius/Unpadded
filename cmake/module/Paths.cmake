4ca93a3530de66c01db98c40baae52f92b7b00bb
StarQTius <dussautoirp@gmail.com>
2025-08-03 21:45:16 +0200

    Remove TOUCH_COMMAND

--- cmake/module/Paths.cmake
+++ cmake/module/Paths.cmake
@@ -2,4 +2,3 @@ cmake_path(APPEND MODULE_PATH ${PROJECT_SOURCE_DIR} cmake module)
 
 cmake_path(APPEND IWYU_LINTER_SCRIPT ${PROJECT_SOURCE_DIR} cmake script
            run-include-what-you-use.cmake)
-cmake_path(APPEND TOUCH_SCRIPT ${PROJECT_SOURCE_DIR} cmake script touch.cmake)
