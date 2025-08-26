07d5cece7b62d990f999ab38e091e5edc61852e2
StarQTius <dussautoirp@gmail.com>
2025-08-26 22:40:38 +0200

    Rewrite CI

---
+++ .github/install/entrypoint.sh
@@ -0,0 +1,12 @@
+#!/bin/sh -l
+
+cd /home/ubuntu
+
+cmake --preset gcc -S "$GITHUB_WORKSPACE" -B build
+cmake --build build --target check_full --parallel $(nproc)
+cmake --preset clang -S "$GITHUB_WORKSPACE" -B build_clang
+cmake --build build_clang --target check_full --parallel $(nproc)
+cmake --build build_clang --target clang_format --parallel $(nproc)
+cmake --build build_clang --target iwyu --parallel $(nproc)
+cmake --build build_clang --target clang_tidy --parallel $(nproc)
+
