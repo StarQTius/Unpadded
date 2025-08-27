dc12c3816a344e7026664d47c66ebd81116a37e7
StarQTius <dussautoirp@gmail.com>
2025-08-28 01:30:30 +0200

    Short-circuit steps in CI

--- .github/install/entrypoint.sh
+++ .github/install/entrypoint.sh
@@ -5,10 +5,10 @@ cd /home/ubuntu
 export CLICOLOR_FORCE=1
 
 cmake --preset gcc -S "$GITHUB_WORKSPACE" -B build
-cmake --build build --target check_full --parallel $(nproc)
-cmake --preset clang -S "$GITHUB_WORKSPACE" -B build_clang
-cmake --build build_clang --target check_full --parallel $(nproc)
-cmake --build build_clang --target clang_format --parallel $(nproc)
-cmake --build build_clang --target iwyu --parallel $(nproc)
-cmake --build build_clang --target clang_tidy --parallel $(nproc)
+&& cmake --build build --target check_full --parallel $(nproc)
+&& cmake --preset clang -S "$GITHUB_WORKSPACE" -B build_clang
+&& cmake --build build_clang --target check_full --parallel $(nproc)
+&& cmake --build build_clang --target clang_format --parallel $(nproc)
+&& cmake --build build_clang --target iwyu --parallel $(nproc)
+&& cmake --build build_clang --target clang_tidy --parallel $(nproc)
 
